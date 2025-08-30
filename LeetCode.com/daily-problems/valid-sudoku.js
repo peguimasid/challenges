73b3289102f6231ad714d62a8a5d11d5b732a6a4
Guilhermo Masid <guilhermomasid@gmail.com>
2025-08-30 17:07:20 -0300

    feat: validate rowns and columns

--- LeetCode.com/daily-problems/valid-sudoku.js
+++ LeetCode.com/daily-problems/valid-sudoku.js
@@ -2,9 +2,31 @@
  * @param {character[][]} board
  * @return {boolean}
  */
+const isValidRowsAndColumns = (board) => {
+  for (let i = 0; i < 9; i++) {
+    let row = [];
+    let col = [];
+    for (let j = 0; j < 9; j++) {
+      row.push(board[i][j]);
+      col.push(board[j][i]);
+    }
+    const rowNums = row.filter((i) => i !== ".");
+    const colNums = col.filter((i) => i !== ".");
+    if (rowNums.length !== [...new Set(rowNums)].length) return false;
+    if (colNums.length !== [...new Set(colNums)].length) return false;
+  }
+
+  return true;
+};
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
 function isValidSudoku(board) {
-  // 1. Validate row and column is easy
-  // 2. Create a function to get sub-boxes and validate non repetition
+  if (!isValidRowsAndColumns(board)) return false;
+
+  return true;
 }
 
 const board1 = [
@@ -33,4 +55,4 @@ const board2 = [
   [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ];
 
-console.log(isValidSudoku(board2)); // false
+// console.log(isValidSudoku(board2)); // false
