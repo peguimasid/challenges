38e8d6b6f71b5f75691b23e8648147ec39594731
Guilhermo Masid <guilhermomasid@gmail.com>
2026-01-01 00:49:52 -0300

    other solution

--- LeetCode.com/daily-problems/plus-one.js
+++ LeetCode.com/daily-problems/plus-one.js
@@ -3,12 +3,21 @@
  * @return {number[]}
  */
 function plusOne(digits) {
-  return String(BigInt(digits.join("")) + 1n)
-    .split("")
-    .map(Number);
+  let i = digits.length - 1;
+
+  while (digits[i] === 9) {
+    digits[i] = 0;
+    if (i === 0) digits.unshift(1);
+    i--;
+  }
+
+  if (i >= 0) digits[i]++;
+
+  return digits;
 }
 
-console.log(plusOne([1, 2, 3])); // [1,2,4]
-console.log(plusOne([4, 3, 2, 1])); // [4,3,2,2]
 console.log(plusOne([9])); // [1,0]
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+console.log(plusOne([9, 9, 9, 9])); // [1,0,0,0,0]
+console.log(plusOne([9, 9, 9, 8])); // [9,9,9,9]
+console.log(plusOne([1, 0, 0, 9])); // [1,0,1,0]
+console.log(plusOne([4, 3, 2, 1])); // [4,3,2,2]
