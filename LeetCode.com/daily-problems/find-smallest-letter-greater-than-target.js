160d727e5730959d6de5365dc0873b3bea53cb81
Guilhermo Masid <guilhermomasid@gmail.com>
2026-01-31 18:53:24 -0300

    improve solution

--- LeetCode.com/daily-problems/find-smallest-letter-greater-than-target.js
+++ LeetCode.com/daily-problems/find-smallest-letter-greater-than-target.js
@@ -7,13 +7,7 @@ function nextGreatestLetter(letters, target) {
   let result = null;
 
   for (const char of letters) {
-    if (!result && char.codePointAt() > target.codePointAt()) {
-      result = char;
-    }
-    if (
-      char.codePointAt() > target.codePointAt() &&
-      char.codePointAt() < result.codePointAt()
-    ) {
+    if (char > target && (result === null || char < result)) {
       result = char;
     }
   }
